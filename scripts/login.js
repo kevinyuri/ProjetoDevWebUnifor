adb888983af59202fc82c5742edaf769e7cd75de
kevinyuri <85417564+kevinyuri@users.noreply.github.com>
2024-06-07 21:23:58 -0300

    ajustes

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,24 @@
-function login() {
+async function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
 
-    // Example hard-coded credentials for demonstration purposes
-    const validUsername = 'user';
-    const validPassword = 'password';
+    const response = await fetch('https://uniforusuariosproject.azurewebsites.net/v1/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
 
-    if (username === validUsername && password === validPassword) {
+    if (response.ok) {
+        const data = await response.json();
+        const username = data.nome;
         localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('loggedInUser', username);
         // Redirect to the home page if credentials are correct
         window.location.href = 'home.html';
     } else {
-        // Display an error message if credentials are incorrect
-        errorMessage.textContent = 'Invalid username or password';
+        errorMessage.textContent = 'Usuário ou senha inválida';
     }
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+let isUpdate = false;
 async function fetchData() {
     const response = await fetch(
       "https://uniforusuariosproject.azurewebsites.net/v1/usuarios"
@@ -8,10 +9,21 @@ async function fetchData() {
 
 function criarUsuario(){
     document.getElementById('userModal').classList.remove('hidden');
+    if (!isUpdate){
+        document.getElementById('criarModal').innerText = 'Criar Conta'
+    }
+    else {
+        document.getElementById('criarModal').innerText = 'Editar'
+    }
 }
 
 function closeModal() {
     document.getElementById('userModal').classList.add('hidden');
+    isUpdate = false;
+    document.getElementById('nome').value = '';
+    document.getElementById('sobrenome').value = '';
+    document.getElementById('username').value = '';
+    document.getElementById('senha').value = '';
 }
 
 async function handle() {
@@ -19,6 +31,11 @@ async function handle() {
     const sobrenome = document.getElementById('sobrenome').value;
     const username = document.getElementById('username').value;
     const senha = document.getElementById('senha').value;
+    const idUser = document.getElementById('idUser').value;
+
+    if (idUser){
+        isUpdate = true;
+    }
 
     if (!nome || !sobrenome || !username || !senha) {
         Swal.fire({
@@ -33,19 +50,34 @@ async function handle() {
         nome,
         sobrenome,
         username,
-        senha
+        senha,
+        idUser
     };
 
     try {
-        await createUser(userData);
-        closeModal();
-        Swal.fire({
-            title: "Usuário criado!",
-            text: "O usuário foi criado com sucesso.",
-            icon: "success"
-        }).then(() => {
-            location.reload();
-        });
+        if (!isUpdate){
+            await createUser(userData);
+            closeModal();
+            Swal.fire({
+                title: "Usuário criado!",
+                text: "O usuário foi criado com sucesso.",
+                icon: "success"
+            }).then(() => {
+                location.reload();
+            });
+        }
+        else {
+            await updateUser(userData);
+            closeModal();
+            Swal.fire({
+                title: "Usuário editado!",
+                text: "O usuário foi editado com sucesso.",
+                icon: "success"
+            }).then(() => {
+                location.reload();
+            });
+        }
+        
     } catch (error) {
         Swal.fire({
             title: "Erro",
@@ -69,6 +101,29 @@ async function createUser(data) {
             throw new Error('Erro ao criar usuário');
         }
         return response.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+async function updateUser(data) {
+    const id = data.idUser;
+    delete data.idUser;
+    console.log(id);
+
+    try {
+        const response = await fetch(`https://uniforusuariosproject.azurewebsites.net/v1/usuarios/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error('Erro ao editar usuário');
+        }
+        isUpdate = false;
+        return
     } catch (error) {
         throw new Error(error.message);
     }
@@ -118,6 +173,31 @@ async function deleteUser(id) {
     });
 }
 
+async function editUserModal(id) {
+    try {
+        isUpdate = true;
+        criarUsuario();
+        // Fetch user details by ID
+        const response = await fetch(`https://uniforusuariosproject.azurewebsites.net/v1/usuarios/${id}`);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar detalhes do usuário');
+        }
+        const user = await response.json();
+        document.getElementById('nome').value = user.nome;
+        document.getElementById('sobrenome').value = user.sobrenome;
+        document.getElementById('username').value = user.userName;
+        document.getElementById('senha').value = user.senha;
+        document.getElementById('idUser').value = user.id;
+        isUpdate = true;
+    } catch (error) {
+        Swal.fire({
+            title: 'Erro',
+            text: error.message,
+            icon: 'error'
+        });
+    }
+}
+
 async function populateDataTable() {
     const data = await fetchData();
     const table = $("#example")
@@ -133,7 +213,7 @@ async function populateDataTable() {
             orderable: false, // This column won't be sortable
             searchable: false, // This column won't be included in searches
             render: function (data, type, row, meta) {
-                return `<button onclick="editRow(${row.id})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Editar</button>
+                return `<button onclick="editUserModal(${row.id})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Editar</button>
                         <button onclick="deleteUser(${row.id})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Excluir</button>`;
             },
             },
